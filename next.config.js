e126ff31fdc8178d8ed0e625af33594467d6ab0c
yoitscarolinee <contas@caroline.art.br>
2022-12-22 20:05:15 -0300

    fix: styled components glitch

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  compiler:{
+    styledComponents:true,
+  },
   swcMinify: true,
   env: {
     RANDOM_API: 'abe4e77e-89f9-4082-b77d-ca64add5513d'
